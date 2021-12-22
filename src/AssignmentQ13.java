33ee69385c3bc43c2310f9dbf967dca6fb81feeb
TJM <tj.extremez@gmail.com>
2021-12-21 21:29:32 -0500

    AssignmentQ13.java

---
+++ src/AssignmentQ13.java
@@ -0,0 +1,12 @@
+
+public class AssignmentQ13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;       
+        x = x++ + ++x + x++ + ++x + ++x;       
+        System.out.println("Value of x : " + x);
+        
+	}
+
+}
