9795d5197ab6316c3824ba48eec26c35871e0a95
TJM <tj.extremez@gmail.com>
2022-01-04 19:23:12 -0500

    FindingGreatestOfThreeNumbers

---
+++ src/GreatestOfThree.java
@@ -0,0 +1,25 @@
+
+public class GreatestOfThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 1500;
+		int num2 = 2000;
+		int num3= 3000;
+		if (num1 >= num2) {
+			System.out.println("num1 is greater");
+		} else if (num1 <= num2) {
+			System.out.println("num2 is greater");
+		} else if (num1 >= num3) {
+			System.out.println("num1 is greater");
+		} else if (num1 <= num3) {
+			System.out.println("num3 is greater");
+		} else if (num2 >= num1) {
+			System.out.println("num2 is greater");
+		} else if (num2 <= num1) {
+			System.out.println("num1 is greater");
+		}
+		
+	}
+
+}
