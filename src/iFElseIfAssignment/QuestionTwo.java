5dde271a476c3f596421e6dbf6c84991ebe5ef3c
TJM <tj.extremez@gmail.com>
2022-01-07 21:32:59 -0500

    QuestionTwo

--- src/iFElseIfAssignment/QuestionTwo.java
+++ src/iFElseIfAssignment/QuestionTwo.java
@@ -4,8 +4,21 @@ public class QuestionTwo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int age = 17;
+		int level = 0;
+		String otherStatus = "Invalid status";
 
-	int age = 18;
-	if (age<=18);
-	System.out.println("License can't be issued"); }}
+		if (age >= 16) {
+			if (level == 0) {
+				System.out.println("You have G1 license issued");
+			} else if (level == 1) {
+				System.out.println("You have G2 license issued");
+			} else if (level == 2) {
+				System.out.println("You have G license issued");
+			} else
+				System.out.println(otherStatus);
 
+		}
+	}
+
+}
