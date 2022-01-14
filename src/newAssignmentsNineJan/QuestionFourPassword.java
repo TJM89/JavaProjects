a70b6d2ef9df6a317bae42abe89c8b0891e68e49
TJM <tj.extremez@gmail.com>
2022-01-13 22:42:10 -0500

    lastQuestionPassword

---
+++ src/newAssignmentsNineJan/QuestionFourPassword.java
@@ -0,0 +1,70 @@
+package newAssignmentsNineJan;
+
+import java.util.Scanner;
+
+public class QuestionFourPassword
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String userName = "pivotAdmin";
+		String passwordUsed = "Admin123";
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Please enter the username:");
+		userName = sc.next();
+		System.out.println("Please enter the password:");
+		passwordUsed = sc.next();
+		
+		// first try
+
+		if (userName.equals("pivotAdmin") && passwordUsed.equals("Admin123"))
+
+			System.out.println("You are logged in to the application");
+
+		else if (!userName.equals("pivotAdmin") && !passwordUsed.equals("Admin123")
+				|| userName.equals("pivotAdmin") && !passwordUsed.equals("Admin123")
+				|| !userName.equals("pivotAdmin") && passwordUsed.equals("Admin123"))
+
+			System.out.println("Incorrect User id or password. Try again");
+
+		else
+
+			System.out.println("Maximum attempts reached. Account locked!");
+
+		// second try
+
+		System.out.println("Please enter the username:");
+		userName = sc.next();
+		System.out.println("Please enter the password:");
+		passwordUsed = sc.next();
+		if (userName.equals("pivotAdmin") && passwordUsed.equals("Admin123"))
+
+			System.out.println("You are logged in to the application");
+
+		else if (!userName.equals("pivotAdmin") && !passwordUsed.equals("Admin123")
+				|| userName.equals("pivotAdmin") && !passwordUsed.equals("Admin123")
+				|| !userName.equals("pivotAdmin") && passwordUsed.equals("Admin123"))
+
+			System.out.println("Incorrect User id or password. Try again");
+
+		// third try
+
+		System.out.println("Please enter the username:");
+		userName = sc.next();
+		System.out.println("Please enter the password:");
+		passwordUsed = sc.next();
+		if (userName.equals("pivotAdmin") && passwordUsed.equals("Admin123"))
+
+			System.out.println("You are logged in to the application");
+
+		else
+
+			System.out.println("Maximum attempts reached. Account locked!");
+
+	}
+}
+
+
