e8ea83ad003458bd903f26249251f29b57b71397
TJM <tj.extremez@gmail.com>
2022-01-19 10:42:59 -0500

    NewAssignmentCompleted

---
+++ src/AssignmentJanuary14/Question3Phones.java
@@ -0,0 +1,46 @@
+package AssignmentJanuary14;
+
+import java.util.Scanner;
+
+public class Question3Phones {
+
+	public static void main(String[] args) {
+
+		String phoneName = "";
+		Scanner sc = new Scanner(System.in);
+
+		// array of phones are created below
+		String samsungPhones[] = { "S20", "S21", "Flip3", "Fold3" };
+		
+		String googlePhones[] = { "Googlepixel 6", "Googlepixel 6 pro", "Googlepixel pro", "Googlepixel 5" };
+
+		String iPhones[] = { "iPhone12", "iPhone 12 mini", "iPhone11", "iPhone 11 mini" };
+
+		System.out.println("Please enter the desired phone name: ");
+		phoneName = sc.next();
+
+			for(int i=0;i<samsungPhones.length;i++)
+			if (samsungPhones[i].equals(phoneName))
+
+			{
+				System.out.println("Product selected is " + samsungPhones[i]);
+				break;
+
+			} else if (googlePhones[i].equals(phoneName)) {
+				System.out.println("Product selected is " + googlePhones[i]);
+				break;
+			}
+
+			else if (iPhones[i].equals(phoneName)) {
+				System.out.println("Product selected is " + iPhones[i]);
+				break;
+			}
+
+			else {
+				System.out.println("Invalid entry, please select from the list");
+				break;
+			}
+
+	}
+
+}
