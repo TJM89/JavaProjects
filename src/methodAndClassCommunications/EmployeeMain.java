d6488a0a2e30f9e8e8427ae145c278398bbefbcc
TJM <tj.extremez@gmail.com>
2022-01-25 16:26:42 -0500

    UpdatedValues

--- src/methodAndClassCommunications/EmployeeMain.java
+++ src/methodAndClassCommunications/EmployeeMain.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class EmployeeMain {
 
 	public static void main(String[] args) {
-		
+
 		double noOfHoursWorked = 0.0;
 		double hourlyRatenew = 0.0;
-		
+
 		EmployeeData firstNameobj = new EmployeeData();
 		EmployeeData lastNameobj = new EmployeeData();
 		EmployeeData hourlyRateobj = new EmployeeData();
@@ -24,12 +24,12 @@ public class EmployeeMain {
 		System.out.println("Hourly pay rate of " + firstName.concat(lastName) + " is $" + hourlyRate);
 
 		firstName = "";
-		firstNameobj.setfirstName("James");
+		firstNameobj.setfirstName("Riyaz");
 		firstName = firstNameobj.getfirstName();
 		System.out.println("Updated first name of employee to " + firstName);
 
 		lastName = "";
-		lastNameobj.setlastName("Swan");
+		lastNameobj.setlastName("Khan");
 		lastName = lastNameobj.getlastName();
 		System.out.println("Updated last name of employee to " + lastName);
 
@@ -38,15 +38,15 @@ public class EmployeeMain {
 		hourlyRate = hourlyRateobj.hourlyRate();
 		System.out.println("Updated hourly pay rate of " + firstName.concat(lastName) + " is $" + hourlyRate);
 
-	Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-	System.out.println("Enter number of hours worked :");
-	noOfHoursWorked = sc.nextDouble();
+		System.out.println("Enter number of hours worked by " + firstName.concat(lastName) + " :" );
+		noOfHoursWorked = sc.nextDouble();
 
-	System.out.println("Enter the hourly rate of the employee :");
-	hourlyRate = sc.nextDouble();
-	
-	System.out.println("Weekly salary of " + firstName.concat(lastName)+ " is $" + noOfHoursWorked * hourlyRate);
+		System.out.println("Enter the hourly rate of the employee :");
+		hourlyRate = sc.nextDouble();
 
-}
+		System.out.println("Weekly salary of " + firstName.concat(lastName) + " is $" + noOfHoursWorked * hourlyRate);
+
+	}
 }
