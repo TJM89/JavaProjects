434c098982f26a95f7c4b39a21e4d580c6f6be67
TJM <tj.extremez@gmail.com>
2022-01-25 23:47:12 -0500

    UpdatedClassValues

---
+++ src/methodAndClassCommunications/ProductDetails.java
@@ -0,0 +1,21 @@
+package methodAndClassCommunications;
+
+public class ProductDetails {
+
+	String laptopName = "The laptop brand is HP";
+	String laptopDescreiption = "It has 12GB RAM, 1TB SSD and runs on Windows 11 OS";
+	double laptopPrice = 999;
+	
+void laptopDetails() {		
+		
+		System.out.println(laptopName);
+		System.out.println(laptopDescreiption);
+		System.out.println("The price of this laptop is $" + laptopPrice);
+		
+	}
+	
+	
+	
+	
+	
+}
