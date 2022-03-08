78d2576d88c0ad3f2a422f3220590eb8e96b0375
TJM <tj.extremez@gmail.com>
2022-03-08 08:28:08 -0500

    newassignment

--- src/stringAssignments/Parts1to3.java
+++ src/stringAssignments/Parts1to3.java
@@ -6,33 +6,30 @@ public class Parts1to3 {
 
 	public static void main(String[] args) {
 
-		//Part 1
-		String cityNames[] = new String[] {"Toronto","Malton","Whitby","Cochin"};
-		
-		//Part2
+		// Part 1
+		String cityNames[] = new String[] { "Toronto", "Malton", "Whitby", "Cochin" };
+
+		// Part2
 		String cityName = "Toronto,Malton,Whitby,Cochin";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a city name : ");
 		String city = sc.next();
-		
-		if(cityName.contains(city)){
+
+		if (cityName.contains(city)) {
 			System.out.println("City name found");
 		} else {
-		System.out.println("City not found in the list");
-			
+			System.out.println("City not found in the list");
+
 		}
-		
-		//Part3
+
+		// Part3
 		String cityNaming = "Toronto";
-		
-		if(cityNaming.startsWith("T")) {
+
+		if (cityNaming.startsWith("T")) {
 			cityNaming.replace("T", "Mississauga");
-			
+
 		}
-		
-		
-		
-		
+
 	}
 
 }
